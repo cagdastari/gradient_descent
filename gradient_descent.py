0fe52ae8c14fca14e8f2c10c254ab9023a5a176e
cagdas9957 <cagdas9957@gmail.com>
2022-09-01 00:07:48 +0300

    edited

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from itertools import zip_longest
+from itertools import count, zip_longest
 
 
 # create random data with help of uniform distrubition
@@ -11,18 +11,19 @@ x_test=np.random.uniform(low=0.5, high=13.3, size=(200,))
 y_test=x_test*12
 
 
-# Graph output of this data is a linear func, so hypothesis func should look like;
+''' 
+Graph output of this data is a linear func, so hypothesis func should look like;
 # h(x)= Q(0) + Q(1)*x
+'''
 
-# initialize values of q_0 and q_1
-q_0=0
-q_1=0
+# initialize parameters q_0 and q_1
+q_0,q_1=0,0 
 
 # empty list to append error values for each q_0 and q_1
 cost=[]
 
 # set proper learning rate
-a=0.1
+a=0.01
 
 # create an empty figure 
 plt.figure()
@@ -35,26 +36,43 @@ def cost_func(q_0,q_1):
     cost.append(sum)
 
 # calculate q_0,q_1    
-def gradient_descent(a,x,y,q_0,q_1):
-    q_0= q_0 - a*1/1000*(q_0+q_1*x-y)
-    q_1= q_1 - a*1/1000*(q_0+q_1*x-y)*x
-    return q_0,q_1
+def grad(q_0,q_1):
+    derivative_0=0
+    derivative_1=0
+    for (x,y) in zip_longest(x_learn,y_learn):
+        derivative_0 = derivative_0 + 1/1000*(q_0+q_1*x-y)
+        derivative_1 = derivative_1+ 1/1000*(q_0+q_1*x-y)*x
+    list=[derivative_0,derivative_1]
+    return list
 
 # test calculated values 
 def test(x,y,q_0,q_1):
     pre_value= q_0 + q_1*x
-    accuracy_score= (np.absolute(pre_value - y)/y)*100
+    accuracy_score= int(100-(np.absolute(pre_value - y)/y)*100)
     print(pre_value,y,accuracy_score)
+    return accuracy_score
 
-
-# run learn algorithms
-for (x,y) in zip_longest(x_learn,y_learn):
-        q_0,q_1=gradient_descent(a,x,y,q_0,q_1)
+# run learning algorithm
+def descent(q_0,q_1,a):
+    count=0
+    while True:
+        count += 1
+        print(count)
+        q_0 = q_0-a*grad(q_0,q_1)[0]
+        q_1 = q_1-a*grad(q_0,q_1)[1]
+        print(q_0,q_1)
         cost_func(q_0,q_1)
+        if count>1000:
+            break
+    return q_0,q_1
+
+q_0,q_1=descent(q_0,q_1,a)
 
 # run test algortihm
+acc=0
 for (x,y) in zip_longest(x_test,y_test):
-    test(x,y,q_0,q_1)
+    acc=acc+test(x,y,q_0,q_1)
+acc=acc/200
 
 # plot the cost func
 plt.plot(cost)
@@ -62,6 +80,7 @@ plt.xlabel('iterations')
 plt.ylabel('cost values')
 plt.title('Cost Function')
 plt.show()
+print(f'q_0:{q_0},q_1:{q_1},mean accuracy:{acc}')
 
 
 
